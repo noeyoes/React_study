214d1803fb84bd493a55bc54566ff6eb98a8ff94
seoyon <lsy021128@naver.com>
2022-08-22 15:21:27 +0900

    02-2 깃허브에 리액트 앱 업로드하기

--- movie_app_2022/src/index.js
+++ movie_app_2022/src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDom from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDom.render(
+  <App /> , document.getElementById('root') // 아이디가 'root'인 엘리먼트에 App 컴포넌트를 그린다고 생각
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
