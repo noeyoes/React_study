05b15cc2be2e6e05d326cbaf36f739139f30b1f2
seoyon <lsy021128@naver.com>
2022-08-24 14:34:29 +0900

    220824

--- movie_app_2022/src/App.js
+++ movie_app_2022/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+function Food({fav}) {
+  return <h1>I like { fav } </h1>;
+}
+
 function App() {
-  return <div>Hello!!</div>
+  return(
+    <div>
+      <h1>Hello</h1>
+      <Food fav = "pizza" />
+      <Food fav = "chicken" />
+      <Food fav = "candy" />
+      <Food fav = "jelly" />
+    </div>
+  );
 }
 
 export default App;
--- movie_app_2022/src/index.js
+++ movie_app_2022/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './App';
+import App from './App'; // App 컴포넌트 임포트하여 사용
 
 ReactDom.render(
   <App /> , document.getElementById('root') // 아이디가 'root'인 엘리먼트에 App 컴포넌트를 그린다고 생각
